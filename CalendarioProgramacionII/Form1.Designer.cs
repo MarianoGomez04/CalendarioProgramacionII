75d395547315c8ffc288055aac7acd221edfdd9a
MarianoGomez04 <mgomezdisalvo@gmail.com>
2022-04-25 19:35:51 -0300

    Agregar archivos de proyecto.

---
+++ CalendarioProgramacionII/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CalendarioProgramacionII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.bCargar = new System.Windows.Forms.Button();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMostrar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(52, 72);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(100, 20);
+            this.txtFecha.TabIndex = 1;
+            // 
+            // bCargar
+            // 
+            this.bCargar.Location = new System.Drawing.Point(524, 72);
+            this.bCargar.Name = "bCargar";
+            this.bCargar.Size = new System.Drawing.Size(75, 23);
+            this.bCargar.TabIndex = 2;
+            this.bCargar.Text = "Cargar";
+            this.bCargar.UseVisualStyleBackColor = true;
+            this.bCargar.Click += new System.EventHandler(this.bCargar_Click);
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(195, 72);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(100, 20);
+            this.txtMes.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mes";
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(330, 72);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(100, 20);
+            this.txtAño.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(327, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Año";
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.AutoSize = true;
+            this.lblMostrar.Location = new System.Drawing.Point(52, 207);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(0, 13);
+            this.lblMostrar.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMostrar);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bCargar);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Button bCargar;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMostrar;
+    }
+}
+
---
+++ CalendarioProgramacionII/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalendarioProgramacionII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void bCargar_Click(object sender, EventArgs e)
+        {
+            int Dias;
+            int Mes1;
+            int Mes2;
+            int Mes3;
+            int Mes4;
+            int Mes5;
+            int Mes6;
+            int Mes7;
+            int Mes8;
+            int Mes9;
+            int Mes10;
+            int Mes11;
+            int Mes12;
+            int Año;
+
+            Mes1 = 1;
+            Mes2 = 2;
+            Mes3 = 3;
+            Mes4 = 4;
+            Mes5 = 5;
+            Mes6 = 6;
+            Mes7 = 7;
+            Mes8 = 8;
+            Mes9 = 9;
+            Mes10 = 10;
+            Mes11 = 11;
+            Mes12 = 12;
+
+
+            lblMostrar.Text = System.Convert.ToString(txtFecha.Text) + "," + System.Convert.ToString(txtMes.Text) + "," + System.Convert.ToString(txtAño.Text);
+
+            if (Mes1 == 1)
+            {
+                
+                lblMostrar.Text = txtFecha.Text + "," + "de Enero" + "," + txtAño.Text;
+            }
+
+            if (Mes2 == 2)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Febrero" + "," + txtAño.Text;
+            }
+            if (Mes3 == 3)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Marzo" + "," + txtAño.Text;
+            }
+            if (Mes4 == 4)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Abril" + "," + txtAño.Text;
+            }
+            if (Mes5 == 5)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Mayo" + "," + txtAño.Text;
+            }
+            if (Mes6 == 6)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Junio" + "," + txtAño.Text;
+            }
+            if (Mes7 == 7)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Julio" + "," + txtAño.Text;
+            }
+            if (Mes8 == 8)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Agosto" + "," + txtAño.Text;
+            }
+            if (Mes9 == 9)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Septiembre" + "," + txtAño.Text;
+            }
+            if (Mes10 == 10)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Octubre" + "," + txtAño.Text;
+            }
+            if (Mes11 == 11)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Noviembre" + "," + txtAño.Text;
+            }
+            if (Mes12 == 12)
+            {
+
+                lblMostrar.Text = txtFecha.Text + "," + "de Diciembre" + "," + txtAño.Text;
+            }
+
+
+
+
+
+
+        }
+    }
+}
